c17412791419cfdff2580b55492613017a3ef835
sstarmann <kruspeissues@gmail.com>
2024-10-31 00:39:33 -0500

    Actualizar database.php

--- config/database.php
+++ config/database.php
@@ -6,7 +6,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $mongoClient = new MongoDB\Client("mongodb+srv://73571574:73571574@cluster1.mnhaq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1");
 
 try {
-    // Verifica la conexión
+    
     $mongoClient->selectDatabase('admin')->command(['ping' => 1]);
     echo "";
 } catch (Exception $e) {
@@ -14,7 +14,7 @@ try {
     exit;
 }
 
-// Selecciona la base de datos y la colección
-$database = $mongoClient->cluster1; // Cambia 'cluster1' por el nombre de tu base de datos si es diferente
-$clientesCollection = $database->mi_coleccion; // Cambia 'mi_coleccion' por el nombre de tu colección
+
+$database = $mongoClient->cluster1;
+$clientesCollection = $database->mi_coleccion;
 ?>
